bf2d5f47cb874f8937481da4df5fee54c069c9c9
SKE <appybuildmore@gmail.com>
2017-07-14 22:37:23 -0500

    Initial Commit

---
+++ ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by Sain-R Edwards Jr. on 7/14/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+
+}
